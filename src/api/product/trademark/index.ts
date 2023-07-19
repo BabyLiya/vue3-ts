2ba92c5ea06c21468211fc48e4d801cfe74f752d
mll <862767768@qq.com>
2023-07-19 23:13:46 +0800

    feat: 新增品牌upload

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,13 +1,28 @@
 // 品牌管理接口
 import request from '@/utils/request'
-import { TrademarkResponse } from './type'
+import { TrademarkResponse, Trademark } from './type'
 
 enum API {
+  // 获取已有品牌接口
   TRADEMARK_URL = '/admin/product/baseTrademark/',
+  // 获取新增接口
+  ADDTRADEMARK_URL = '/admin/product/baseTrademark/save',
+  // 获取修改接口
+  EDITTRADEMARK_URL = '/admin/product/baseTrademark/update',
 }
 
 export const reqTrademark = (page: number, limit: number) => {
   return request.get<any, TrademarkResponse>(
     API.TRADEMARK_URL + `${page}/${limit}`,
   )
+}
+
+// 添加/修改已有接口的方法
+export const reqAddTrademark = (data: Trademark) => {
+  if (data.id) {
+    // 修改已有商品数据
+    return request.put<any,any>(API.EDITTRADEMARK_URL,data)
+  } else {
+    return request.post<any,any>(API.ADDTRADEMARK_URL,data)
+  }
 }
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <el-card class="box-card">
-      <el-button :icon="Plus" type="primary" size="default" @click="">
+      <el-button
+        :icon="Plus"
+        type="primary"
+        size="default"
+        @click="addTrademark"
+      >
         添加品牌
       </el-button>
       <el-table border :data="trademarkArr">
@@ -27,8 +32,16 @@
         </el-table-column>
         <el-table-column label="品牌操作" align="center">
           <template #="{ row, column, $index }">
-            <el-button :icon="Edit" type="primary" @click=""></el-button>
-            <el-button :icon="Delete" type="danger" @click=""></el-button>
+            <el-button
+              :icon="Edit"
+              type="primary"
+              @click="editTrademark"
+            ></el-button>
+            <el-button
+              :icon="Delete"
+              type="danger"
+              @click="deleteTrademark"
+            ></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -44,15 +57,50 @@
         @current-change="getTrademarkData"
       />
     </el-card>
+    <el-dialog
+      v-model="dialogVisible"
+      title="添加品牌"
+      width="60%"
+    >
+      <el-form label-width="100px" ref="formRef" :model="dataForm" :rules="rules">
+        <el-form-item label="品牌名称" prop="tmName">
+          <el-input
+            v-model="dataForm.tmName"
+            placeholder="请输入品牌名称"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" prop="logoUrl">
+          <!-- action: 图片上传路径，需要带上/api 否则代理服务器不发送post请求 -->
+          <el-upload
+            class="avatar-uploader"
+            action="/api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="dataForm.logoUrl" :src="dataForm.logoUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">
+            确认
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script setup lang="ts">
 import { Delete, Edit } from '@element-plus/icons-vue'
 import { Plus } from '@element-plus/icons-vue'
 import { onMounted, reactive, ref } from 'vue'
-import { reqTrademark } from '@/api/product/trademark/index'
-import type { Records, TrademarkResponse } from '@/api/product/trademark/type'
-
+import { reqTrademark, reqAddTrademark } from '@/api/product/trademark/index'
+import type { Records, TrademarkResponse, Trademark } from '@/api/product/trademark/type'
+import { ElNotification } from 'element-plus'
 // 当前页码
 const currentPage = ref<number>(1)
 // 每页展示多少数据
@@ -61,8 +109,16 @@ let limit = ref<number>(3)
 let total = ref<number>(0)
 
 let background = ref(true)
+let dialogVisible = ref(false)
 
 let trademarkArr = reactive<Records>([])
+
+
+
+const dataForm = reactive({
+  tmName: '',
+  logoUrl: '',
+})
 
 const getTrademarkData = async (pager = 1) => {
   currentPage.value = pager
@@ -76,10 +132,59 @@ const getTrademarkData = async (pager = 1) => {
   }
 }
 
+// 添加品牌
+const addTrademark = () => {
+  dialogVisible.value = true
+}
+
+// 修改品牌
+const editTrademark = () => {
+  dialogVisible.value = true
+}
+
+const rules = {
+  tmName: [
+    {
+      required: true,
+      message: '品牌名称不能为空',
+      trigger: 'blur',
+    },
+  ],
+  logoUrl: [
+    {
+      required: true,
+      message: '品牌logo不能为空',
+      trigger: 'blur',
+    },
+  ],
+}
 // 下拉菜单发生变化时触发
 const handleSizeChange = (e: any) => {
   // 数据量发生变化，页码 -> 1
   getTrademarkData()
+}
+
+// 上传图片组件 -> 在上传图片之前触发， 上传之前可约束文件类型和大小
+const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+  // console.log(rawFile)
+  if (rawFile.type === 'image/png' || rawFile.type === 'image/jpeg' || rawFile.type === 'image/gif') {
+    return true
+  } else {
+    ElNotification({
+    title: 'Error',
+    message: '上传的文件类型应为png|jpeg|gif',
+    type: 'error',
+  })
+  }
+}
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (
+  response: any,
+  uploadFile : any
+) => {
+  // response为当前上传图片post服务器返回的数据
+  console.log(response,"@@@",uploadFile)
+  dataForm.logoUrl = response.data
 }
 
 onMounted(() => {
@@ -92,5 +197,33 @@ onMounted(() => {
   .el-table {
     margin: 10px 0;
   }
+}
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
 }
 </style>
